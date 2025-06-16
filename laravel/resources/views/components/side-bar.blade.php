aa65b81136fd536b00fb618d3c097320be4c8b21
Yuda-pratama695 <if23.yudapratama@ubpkarawang.ac.id>
2025-06-16 21:13:41 +0700

    side-bar

--- laravel/resources/views/components/side-bar.blade.php
+++ laravel/resources/views/components/side-bar.blade.php
@@ -3,71 +3,68 @@
 @endphp
 <div x-data="{ open: false }" class="min-h-screen flex bg-gray-100">
     <!-- Sidebar -->
-    <div :class="open ? 'block' : 'hidden md:block'" class="fixed inset-y-0 left-0 bg-[#5180DB] shadow-md z-30 md:relative md:block transition-all w-full md:w-64 flex flex-col justify-between">
-        <div class="flex items-center justify-between px-3 py-2 border-b m-3 border-black">
-            <div class="relative w-20 h-20 flex items-center justify-center">
-            <!-- Heksagon dengan border -->
-            <div class="absolute w-full h-full clip-hexagon border-4 bg-blue-900"></div>
-
-            <!-- Ikon di tengah -->
-            <span class="iconify w-8 h-8 text-white z-10" data-icon="solar:letter-linear"></span>
+    <div :class="open ? 'block' : 'hidden md:block'" class="fixed inset-y-0 left-0 bg-[#5180DB] shadow-md z-30 md:relative md:block transition-all w-full md:w-64 flex flex-col">
+        <div>
+            <div class="flex items-center justify-between px-3 py-2 border-b m-3 border-black">
+                <div class="relative w-20 h-20 flex items-center justify-center">
+                    <!-- Heksagon dengan border -->
+                    <div class="absolute w-full h-full clip-hexagon border-4 bg-blue-900"></div>
+                    <!-- Ikon di tengah -->
+                    <span class="iconify w-8 h-8 text-white z-10" data-icon="solar:letter-linear"></span>
+                </div>
+                <span class="font-quicksand font-bold text-3xl text-white">E-SURAT</span>
+                <button @click="open = false" class="md:hidden text-white">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+                    </svg>
+                </button>
             </div>
-            <span class="font-quicksand font-bold text-3xl text-white">E-SURAT</span>
-            <button @click="open = false" class="md:hidden text-white">
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
-                </svg>
-            </button>
+            <nav class="mt-6">
+                <ul class="space-y-3">
+                    <li>
+                        <a href="/"
+                           class="block px-6 py-2 border-b transition rounded-xl m-3 font-semibold
+                                  {{ request()->is('/') ? 'border-white bg-white text-black' : 'border-black bg-[#2950A4] text-white hover:bg-[#355AA9] hover:text-white' }}">
+                            Dashboard
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/submissions"
+                           class="block px-6 py-2 border-b transition rounded-xl m-3 font-semibold
+                                  {{ request()->is('submissions') ? 'border-white bg-white text-black' : 'border-black bg-[#2950A4] text-white hover:bg-[#355AA9] hover:text-white' }}">
+                            Pengajuan Surat
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/accounts"
+                           class="block px-6 py-2 border-b transition rounded-xl m-3 font-semibold
+                                  {{ request()->is('accounts') ? 'border-white bg-white text-black' : 'border-black bg-[#2950A4] text-white hover:bg-[#355AA9] hover:text-white' }}">
+                            Buat Akun
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/profile"
+                           class="block px-6 py-2 border-b transition rounded-xl m-3 font-semibold
+                                  {{ request()->is('profile') ? 'border-white bg-white text-black' : 'border-black bg-[#2950A4] text-white hover:bg-[#355AA9] hover:text-white' }}">
+                            Profile
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
-        <nav class="mt-6 mb-6">
-            <ul class="space-y-3">
-                <li>
-                    <a href="/"
-                       class="block px-6 py-2 border-b transition rounded-xl m-3 font-semibold
-                              {{ request()->is('/') ? 'border-white bg-white text-black' : 'border-black bg-[#2950A4] text-white hover:bg-[#355AA9] hover:text-white' }}">
-
-                        Dashboard
-                    </a>
-                </li>
-                <li>
-                    <a href="/submissions"
-                       class="block px-6 py-2 border-b transition rounded-xl m-3 font-semibold
-                              {{ request()->is('submissions') ? 'border-white bg-white text-black' : 'border-black bg-[#2950A4] text-white hover:bg-[#355AA9] hover:text-white' }}">
-                        Pengajuan Surat
-                    </a>
-                </li>
-                <li>
-                    <a href="/accounts"
-                       class="block px-6 py-2 border-b transition rounded-xl m-3 font-semibold
-                              {{ request()->is('accounts') ? 'border-white bg-white text-black' : 'border-black bg-[#2950A4] text-white hover:bg-[#355AA9] hover:text-white' }}">
-                        Buat Akun
-                    </a>
-                </li>
-                <li>
-                    <a href="/profile"
-                       class="block px-6 py-2 border-b transition rounded-xl m-3 font-semibold
-                              {{ request()->is('profile') ? 'border-white bg-white text-black' : 'border-black bg-[#2950A4] text-white hover:bg-[#355AA9] hover:text-white' }}">
-                        Profile
-                    </a>
-                </li>
-                <li>
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <button href="/logout" type="submit"
-                        class="block mt-80 px-6 py-2 border-b border-black bg-red-600 hover:bg-red-700 text-white font-semibold transition rounded-xl m-3"
-                        :class="{ 'bg-white text-black': $el === document.activeElement }"
-                        @click="$el.blur()">
-                            Logout
-                        </button>
-                    </form>
-                </li>
-            </ul>
-        </nav>
+        <!-- Logout di pojok kiri bawah -->
+        <div class="mt-auto mb-6">
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button type="submit"
+                    class="block px-6 py-2 border-b border-black bg-red-600 hover:bg-red-700 text-white font-semibold transition rounded-xl m-3 text-left">
+                    Logout
+                </button>
+            </form>
+        </div>
     </div>
-
     <!-- Overlay for mobile -->
     <div x-show="open" @click="open = false" class="fixed inset-0 bg-black bg-opacity-30 z-20 md:hidden"></div>
-
     <!-- Main content -->
     <div class="flex-1 flex flex-col">
         <!-- Topbar for mobile -->
@@ -80,8 +77,7 @@
             <span class="font-bold text-lg text-[#2950A4]">E-Surat</span>
         </header>
         <main class="p-6">
-        <x-profile-header></x-profile-header>
-
+            <x-profile-header></x-profile-header>
             <!-- Konten utama di sini -->
             <h1 class="text-2xl font-bold mb-4">{{ $heading }}</h1>
             {{ $slot }}
